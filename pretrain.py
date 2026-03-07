a07b2e22ca5e28bfd34dcddc95fb94c5892c5cbd
Tad T <tadtheyounger@gmail.com>
2026-03-07 13:16:46 -0600

    remove AdamATan2 and replace with AdamW in pretrain.py, and update imports accordingly.

--- pretrain.py
+++ pretrain.py
@@ -16,7 +16,7 @@ from coolname.impl import generate_slug
 import hydra
 import pydantic
 from omegaconf import DictConfig
-from adam_atan2 import AdamATan2
+from torch.optim import AdamW
 
 from puzzle_dataset import PuzzleDataset, PuzzleDatasetConfig, PuzzleDatasetMetadata
 from utils.functions import load_model_class, get_model_source_path
@@ -143,7 +143,7 @@ def create_model(config: PretrainConfig, train_metadata: PuzzleDatasetMetadata, 
 
             world_size=world_size
         ),
-        AdamATan2(
+        AdamW(
             model.parameters(),
 
             lr=0,  # Needs to be set by scheduler
